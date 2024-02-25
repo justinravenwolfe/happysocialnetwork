198a6e5c666a7eeb7e84f52dcb27d3f015decd43
justinravenwolfe <147559260+justinravenwolfe@users.noreply.github.com>
2024-02-25 12:18:01 -0500

    Created reaction model and json keys, reactionID, reactionBody, username and createdAt. Each key is associated with values that have been put in place

---
+++ model/reaction.js
@@ -0,0 +1,28 @@
+//Code for the reaction model 
+const reactionSchema = new mongoose.Schema({
+    reactionId: {
+        type: mongoose.Schema.Types.ObjectId, 
+        default: () => new mongoose.Types.ObjectId()
+    },
+    reactionBody: {
+        type: String, 
+        required: true,
+        minLength: 1, 
+        maxLength: 280,
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createAtVal => dateFormat(createAtVal)
+    }
+}); 
+
+const reaction = mongoose.model('reaction', reactionSchema); 
+/*
+Users -> [user, users] -> friends list -> list of thoughts [thoughts, thoughts, thoughts]
+->thoughts -> reactions
+*/
